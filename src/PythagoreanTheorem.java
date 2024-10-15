645d6e99f973191fd89b1680847cd4a41c3e02a0
nick <nikdanelia@gmail.com>
2024-10-15 21:20:32 +0400

    pythagorean theorem

--- src/PythagoreanTheorem.java
+++ src/PythagoreanTheorem.java
@@ -1,15 +1,16 @@
-/*
- * File: PythagoreanTheorem.java
- * Name: 
- * Section Leader: 
- * -----------------------------
- * This file is the starter file for the PythagoreanTheorem problem.
- */
-
-import acm.program.*;
+import acm.program.ConsoleProgram;
 
 public class PythagoreanTheorem extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
+    public void run() {
+        println("Enter numbers to compute Pythagorean theorem");
+        int a = readInt("a: ");
+        while (a <= 0) {
+            a = readInt("a has to be positive integer: ");
+        }
+        int b = readInt("b: ");
+        while (b <= 0) {
+            b = readInt("b has to be positive integer: ");
+        }
+        print("c = " + Math.sqrt(a * a + b * b));
+    }
 }
